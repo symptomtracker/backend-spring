d77cdf65c6b8d202c13c99ddb4593af0ff25088b
Alexander Chmielewski <alexander_chmielewski@hotmail.com>
2020-03-28 09:14:53 -0700

    Playing around

--- src/main/java/de/symptromtracker/backend/spring/SymptomtrackerbackendspringApp.java
+++ src/main/java/de/symptromtracker/backend/spring/SymptomtrackerbackendspringApp.java
@@ -14,6 +14,7 @@ import org.springframework.boot.autoconfigure.liquibase.LiquibaseProperties;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.core.env.Environment;
 
 import javax.annotation.PostConstruct;
@@ -26,6 +27,7 @@ import java.util.Collection;
 @EnableConfigurationProperties({LiquibaseProperties.class, ApplicationProperties.class})
 @EnableDiscoveryClient
 @EnableZuulProxy
+@ComponentScan
 public class SymptomtrackerbackendspringApp {
 
     private static final Logger log = LoggerFactory.getLogger(SymptomtrackerbackendspringApp.class);
